834de1a53476bc0aa56ac45f002e51235b729dfc
Derol Willis <derolwillis@example.com>
2026-08-30 16:39:26 +0200

    feat: add capacitor config and android build setup

---
+++ android/app/src/main/java/com/calcuboss/os6/MainActivity.java
@@ -0,0 +1,5 @@
+package com.calcuboss.os6;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.calcuboss.os6',
+  appName: 'Calcuboss OS6',
+  webDir: 'dist'
+};
+
+export default config;
